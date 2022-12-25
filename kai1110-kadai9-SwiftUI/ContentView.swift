dec907d5982771eaaeafeec562c2bd7f5698d028
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2022-12-25 16:17:36 +0900

    FBを反映

--- kai1110-kadai9-SwiftUI/ContentView.swift
+++ kai1110-kadai9-SwiftUI/ContentView.swift
@@ -10,7 +10,7 @@ struct ContentView: View {
             Text("都道府県")
             Text("\(selection)")
             Button(action: {
-                isPresentSelectionView.toggle()
+                isPresentSelectionView = true
             }) {
                 Text("入力")
             }
@@ -25,15 +25,23 @@ struct ContentView: View {
 struct SelectionView: View {
     @Binding var isPresentSelectionView: Bool
     @Binding var selection: String
+    let prefectures = ["東京都", "神奈川県", "埼玉県", "千葉県"]
     
     var body: some View {
         NavigationStack {
             VStack(spacing: 30) {
-                selectionButtonView(isPresentSelectionView: $isPresentSelectionView, selection: $selection, prefecture: "東京都")
-                selectionButtonView(isPresentSelectionView: $isPresentSelectionView, selection: $selection, prefecture: "神奈川県")
-                selectionButtonView(isPresentSelectionView: $isPresentSelectionView, selection: $selection, prefecture: "埼玉県")
-                selectionButtonView(isPresentSelectionView: $isPresentSelectionView, selection: $selection, prefecture: "千葉県")
-                Spacer()
+                ForEach(prefectures, id: \.self) { prefecture in
+                    Button(action: {
+                        selection = prefecture
+                        isPresentSelectionView = false
+                    }) {
+                        Text("\(prefecture)")
+                    }
+                }
+//                selectionButtonView(isPresentSelectionView: $isPresentSelectionView, selection: $selection, prefecture: "東京都")
+//                selectionButtonView(isPresentSelectionView: $isPresentSelectionView, selection: $selection, prefecture: "神奈川県")
+//                selectionButtonView(isPresentSelectionView: $isPresentSelectionView, selection: $selection, prefecture: "埼玉県")
+//                selectionButtonView(isPresentSelectionView: $isPresentSelectionView, selection: $selection, prefecture: "千葉県")
                     .toolbar {
                         ToolbarItem(placement: .navigationBarLeading) {
                             Button(action: {
@@ -48,20 +56,20 @@ struct SelectionView: View {
     }
 }
 
-struct selectionButtonView: View {
-    @Binding var isPresentSelectionView: Bool
-    @Binding var selection: String
-    let prefecture: String
-    
-    var body: some View {
-        Button(action: {
-            selection = prefecture
-            isPresentSelectionView.toggle()
-        }) {
-            Text("\(prefecture)")
-        }
-    }
-}
+//struct selectionButtonView: View {
+//    @Binding var isPresentSelectionView: Bool
+//    @Binding var selection: String
+//    let prefecture: String
+//
+//    var body: some View {
+//        Button(action: {
+//            selection = prefecture
+//            isPresentSelectionView.toggle()
+//        }) {
+//            Text("\(prefecture)")
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
